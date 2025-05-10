55384a2cb4d31b921885468cf4bee91446a0c1d8
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-05-10 08:50:24 -0300

    feat: add outputs of vpc resources

--- modules/vps/datasources.tf
+++ modules/vps/datasources.tf
@@ -6,18 +6,19 @@ data "aws_vpc" "main" {
   ]
 }
 
-data "aws_security_group" "ssh"  {
-  id = aws_security_group.ssh.id
-
-  depends_on = [
-    aws_security_group.ssh
-  ]
-}
-
 data "aws_subnet" "public" {
   id = aws_subnet.public.id
 
   depends_on = [
     aws_subnet.public,
+  ]
+}
+
+
+data "aws_security_group" "ssh"  {
+  id = aws_security_group.ssh.id
+
+  depends_on = [
+    aws_security_group.ssh
   ]
 }
---
+++ modules/vps/outputs.tf
@@ -0,0 +1,18 @@
+
+output "vpc_id" {
+  value       = data.aws_vpc.main.id
+  sensitive   = true
+  description = "ID of VPC"
+}
+
+output "subnet_id" {
+  value       = data.aws_subnet.public.id
+  sensitive   = false
+  description = "ID subnet"
+}
+
+output "security_group_ids" {
+  value       = data.aws_security_group.ssh.id
+  sensitive   = true
+  description = "ID of SSH"
+}
