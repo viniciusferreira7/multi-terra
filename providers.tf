74ced3905839a0d6e3791529b38d815be6b7ef49
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-05-15 08:17:42 -0300

    feat: add backend to save in s3

--- providers.tf
+++ providers.tf
@@ -5,10 +5,25 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket  = "multi-terra-${terraform.workspace}-state-bucket-tf"
+    region  = "us-east-1"
+    key     = "terraform.tfstate"
+    encrypt = true
+    profile = var.profile
+  }
+
 }
 
-# Configure the AWS Provider
 provider "aws" {
-  region  = "us-east-1"
+ region  = "us-east-1"
   profile = var.profile
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.state_bucket
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
