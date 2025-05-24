c32f7933656bf71e757babe77a41c8ac26289a3b
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-05-24 08:08:20 -0300

    chore: apply improvements to the code

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,28 +1,28 @@
 module "vpc" {
   source         = "../../modules/vpc"
-  vpc_cidr_block = "10.0.0.0/16"
+  vpc_cidr_block = var.vpc_cidr_block
   vpc_tags = {
     Name = "main-vpc"
-    Env  = "dev"
+    Env  = var.environment
   }
 
-  subnet_cidr_block              = "10.0.1.0/24"
-  subnet_availability_zone       = "us-east-1"
+  subnet_cidr_block              = var.subnet_cidr_block
+  subnet_availability_zone       = var.subnet_availability_zone
   subnet_map_public_ip_on_launch = true
   subnet_tags = {
     Name = "public-subnet"
-    Env  = "dev"
+    Env  = var.environment
   }
 
   gateway_tags = {
     Name = "igw"
-    Env  = "dev"
+    Env  = var.environment
   }
 
-  route_cidr_block = "0.0.0.0/0"
+  route_cidr_block = var.route_cidr_block
   route_tags = {
     Name = "public-route"
-    Env  = "dev"
+    Env  = var.environment
   }
 
   ssh_name        = "allow-ssh"
@@ -32,7 +32,7 @@ module "vpc" {
       from_port   = 22
       to_port     = 22
       protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+      cidr_blocks = var.ssh_cidr_blocks
     }
   ]
   egress_rules = [
@@ -45,65 +45,58 @@ module "vpc" {
   ]
   ssh_tags = {
     Name = "ssh-sg"
-    Env  = "dev"
+    Env  = var.environment
   }
 }
 
 module "ec2" {
   source                      = "../../modules/ec2"
-  ami_id                      = "ami-0123456789abcdef0"
-  instance_type               = "t3.micro"
-  key_name                    = "ec2-dev"
+  ami_id                      = var.ami_id
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
   subnet_id                   = module.vpc.subnet_id
   security_group_ids          = [module.vpc.security_group_ids]
   associate_public_ip_address = true
-  iam_instance_profile        = "ec2-s3-role"
+  iam_instance_profile        = var.iam_instance_profile
   user_data                   = <<-EOF
-                                    #!/bin/bash
-                                    sudo apt update -y
-                                    sudo apt install nginx -y
-                                  EOF
+    #!/bin/bash
+    sudo apt update -y
+    sudo apt install nginx -y
+  EOF
   root_volume_size            = 20
   root_volume_type            = "gp3"
   tags = {
-    Name = "Ec2-dev"
-    Env  = "dev"
+    Name = "Ec2-${var.environment}"
+    Env  = var.environment
     Iac  = true
   }
-
-  depends_on = [
-    module.vpc
-  ]
+  depends_on = [module.vpc]
 }
 
 module "ebs" {
   source              = "../../modules/ebs"
   name                = "ebs_instance"
-  availability_zone   = "us-east-1"
+  availability_zone   = var.subnet_availability_zone
   size                = "20"
   ec2_id              = module.ec2.id
   enable_key_rotation = true
-
   tags = {
-    Name = "ebs_instance-dev"
-    Env  = "dev"
+    Name = "ebs_instance-${var.environment}"
+    Env  = var.environment
     Iac  = true
   }
-
-  depends_on = [
-    module.ec2
-  ]
+  depends_on = [module.ec2]
 }
 
 module "alb" {
   source                 = "../../modules/alb"
-  alb_name               = "my-app-alb"
+  alb_name               = var.alb_name
   alb_internal           = false
   alb_type               = "application"
   alb_security_group_ids = [module.vpc.security_group_ids]
   alb_subnet_ids         = [module.vpc.subnet_id]
   target_id              = module.ec2.id
-  target_group_name      = "myapp-tg"
+  target_group_name      = var.target_group_name
   target_group_port      = 80
   target_group_protocol  = "HTTP"
   vpc_id                 = module.vpc.vpc_id
@@ -114,8 +107,8 @@ module "alb" {
   unhealthy_threshold    = 2
   health_check_matcher   = "200-299"
   tags = {
-    Name = "alb-dev"
-    Env  = "dev"
+    Name = "alb-${var.environment}"
+    Env  = var.environment
     Iac  = true
   }
   depends_on = [
@@ -123,5 +116,3 @@ module "alb" {
     module.ec2
   ]
 }
-
-//TODO: Run terraform apply
